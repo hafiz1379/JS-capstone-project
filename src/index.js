9f7d87c97d600e30081de5fa106abff767ebde84
hafiz1379 <hafizrasa1379@gmail.com>
2023-05-31 00:31:31 +0430

    Added API url and functions

--- src/index.js
+++ src/index.js
@@ -1,6 +1,59 @@
 import './style.css';
 import Icon from './images/nitro-logo.png';
+const $ = document;
 
 // Find the logo image element and set its src attribute
-const logoImage = document.querySelector('.logo');
+const logoImage = $.querySelector('.logo');
 logoImage.src = Icon;
+
+
+const apiUrl = 'https://api.tvmaze.com/shows';
+const movieCardsContainer = $.getElementById('movie-cards');
+
+async function fetchMovieData(showId) {
+  const response = await fetch(`${apiUrl}/${showId}`);
+  const data = await response.json();
+  return {
+    name: data.name,
+    image: data.image.medium,
+    summary: data.summary,
+    genres: data.genres,
+  };
+}
+
+function createMovieCard(movieData) {
+  const card = $.createElement('div');
+  card.classList.add('movie-card');
+
+  const title = $.createElement('h2');
+  title.innerText = movieData.name;
+
+  const image = $.createElement('img');
+  image.src = movieData.image;
+  const comment = $.createElement('button');
+  comment.innerHTML = 'Comment';
+
+  const genres = $.createElement('p');
+  genres.innerHTML = `<strong>Genres:</strong> ${movieData.genres.join(', ')}`;
+
+  card.appendChild(title);
+  card.appendChild(image);
+  card.appendChild(genres);
+  card.appendChild(comment);
+  return card;
+}
+
+async function createMovieCards() {
+  const response = await fetch(`${apiUrl}`);
+  const showData = await response.json();
+  const shows = showData.slice(0, 20);
+
+  // Create movie cards for each TV show
+  shows.forEach(async (show) => {
+    const movieData = await fetchMovieData(show.id);
+    const movieCard = createMovieCard(movieData);
+    movieCardsContainer.appendChild(movieCard);
+  });
+}
+
+createMovieCards();
