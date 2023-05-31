f5ee17b0524dafee65529c76d34543af53c29c88
TomasEsquivel <117373513+tomasesquivelgc@users.noreply.github.com>
2023-05-31 14:50:29 -0300

    add input fields to comment card

--- src/modules/comment.js
+++ src/modules/comment.js
@@ -1,38 +1,60 @@
 const showComments = (movieData) => {
   const $ = document;
   const movieCardsContainer = $.getElementById('movie-cards');
+
   const popUp = $.createElement('div');
-  const popCard = $.createElement('div');
-  const newImage = $.createElement('img');
-  const title = $.createElement('h2');
-  const status = $.createElement('p');
-  const premiered = $.createElement('p');
-  const commentTitle = $.createElement('h3');
-  const commentSection = $.createElement('div');
-
-
   popUp.classList.add('popUp');
+
+  const popCard = $.createElement('div');
   popCard.classList.add('popCard');
+
+  const newImage = $.createElement('img');
   newImage.classList.add('commentImage');
+  newImage.src = movieData.image;
+
+  const title = $.createElement('h2');
   title.classList.add('commentTitle');
+  title.innerText = movieData.name;
+
+  const status = $.createElement('p');
+  status.innerHTML = `status: ${movieData.showStatus}`;
+
+  const premiered = $.createElement('p');
+  premiered.innerHTML = `premiered: ${movieData.premiered}`;
+
+  const commentTitle = $.createElement('h3');
+  commentTitle.innerHTML = 'Comments';
+
+  const commentSection = $.createElement('div');
   commentSection.classList.add('commentSection');
 
-  newImage.src = movieData.image;
-  title.innerText = movieData.name;
-  status.innerHTML = `status: `+movieData.showStatus;
-  premiered.innerHTML= `premiered: `+movieData.premiered;
-  commentTitle.innerHTML = 'Comments';
+  const commentAddTitle = $.createElement('h3');
+  commentAddTitle.innerHTML = 'Add a comment';
+
+  const nameInput = $.createElement('input');
+  nameInput.setAttribute('type', 'text');
+  nameInput.setAttribute('placeholder', 'Your Name');
+
+  const messageInput = $.createElement('textarea');
+  messageInput.setAttribute('placeholder', 'Enter your comment/message');
+
+  const sendButton = $.createElement('button');
+  sendButton.innerText = 'Send';
 
   popCard.appendChild(newImage);
   popCard.appendChild(title);
   popCard.appendChild(status);
   popCard.appendChild(premiered);
   popCard.appendChild(commentTitle);
+  popCard.appendChild(commentSection);
+  popCard.appendChild(commentAddTitle);
+  popCard.appendChild(nameInput);
+  popCard.appendChild(messageInput);
+  popCard.appendChild(sendButton);
 
   popUp.appendChild(popCard);
   movieCardsContainer.appendChild(popUp);
-  
-  
 };
+
 
 export default showComments;
