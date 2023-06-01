8347fa78a48f6189c2a7a3e14cdae3a2889df879
TomasEsquivel <117373513+tomasesquivelgc@users.noreply.github.com>
2023-05-31 23:45:14 -0300

    fetchData function finish
    function now displays the comments in the commentSection when the popUp is constructed.
    comments add to the first two element as examples

--- src/modules/loadSaveComment.js
+++ src/modules/loadSaveComment.js
@@ -1,14 +1,19 @@
 const fetchData = async (movieData) => {
   const API = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/X5Vx3TOWxyak1SgHIAAo/comments?item_id=${movieData.id}`;
+  const commentSection = document.querySelector('.commentSection');
 
   try {
     const response = await fetch(API);
     const data = await response.json();
 
     // Process the retrieved data
-    console.log(data);
+    data.forEach(element => {
+      const toAdd = document.createElement('p');
+      toAdd.innerHTML = `${element.creation_date} ${element.username}: ${element.comment}`;
+      commentSection.appendChild(toAdd);
+    });
   } catch (error) {
-    console.error('Error:', error);
+    commentSection.innerHTML = 'No comments yet';
   }
 };
 
