33902fbf4bd2f429b8c4dd636935b050dfa57c59
mdg0410 <levoyersteven@gmail.com>
2023-06-02 04:17:45 -0500

    Add descriptions to the `counting reserves` tests.

--- src/modules/__test__/counterReserve.test.js
+++ src/modules/__test__/counterReserve.test.js
@@ -5,20 +5,20 @@ import {
 } from '../__Mocks__/reservationsMock.js';
 import counterReserveJest from '../__Mocks__/counterReserveMock.js';
 
-describe('', () => {
-  test('', () => {
+describe('Testing reservations counter ', () => {
+  test('The first test movie has four reservations, the function is expected to return the numerical value 4.', () => {
     expect(counterReserveJest(film1)).toBe(4);
   });
 
-  test('', () => {
+  test('The second test movie has five reservations, the function is expected to return the numerical value 5.', () => {
     expect(counterReserveJest(film2)).toBe(5);
   });
 
-  test('', () => {
+  test('The third test movie has one reservations, the function is expected to return the numerical value 1.', () => {
     expect(counterReserveJest(film3)).toBe(1);
   });
 
-  test('', () => {
+  test('The fourth test movie has three reservations, the function is expected to return the numerical value 3.', () => {
     expect(counterReserveJest(film4)).toBe(3);
   });
 });
